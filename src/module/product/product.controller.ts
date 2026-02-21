4b219a5433c1625cd919fdf417007c28dffc3704
ecthon <ecthon@gmailcom>
2026-02-20 22:11:10 -0400

    feat: add zod dependency and refactor ProductController to use service instance methods

--- src/module/product/product.controller.ts
+++ src/module/product/product.controller.ts
@@ -2,14 +2,21 @@ import type { FastifyReply, FastifyRequest } from "fastify"
 import { ProductService } from "./product.service.js"
 
 export class ProductController {
+
+    private service: ProductService
+
+    constructor() {
+        this.service = new ProductService()
+    }
+
     async getProducts(request: FastifyRequest, reply: FastifyReply) {
-        const products = await ProductService.prototype.getProducts()
+        const products = await this.service.getProducts()
         reply.send(products)
     }
 
     async getProductById(request: FastifyRequest, reply: FastifyReply) {
         const id = parseInt((request.params as { id: string }).id)
-        const product = await ProductService.prototype.getProductById(id)
+        const product = await this.service.getProductById(id)
         if (!product) {
             reply.status(404).send({ error: "Product not found" })
             return
@@ -23,7 +30,7 @@ export class ProductController {
             return
         }
         const payload = { name, price, categoryId, active }
-        const newProduct = await ProductService.prototype.createProduct(payload)
+        const newProduct = await this.service.createProduct(payload)
         reply.status(201).send(newProduct)
     }
 
@@ -31,7 +38,7 @@ export class ProductController {
         const id = parseInt((request.params as { id: string }).id)
         const { name, price, categoryId, active } = request.body as { name?: string; price?: number, categoryId?: number, active?: boolean }
         const payload = Object.fromEntries(Object.entries({ name, price, categoryId, active }).filter(([, v]) => v !== undefined))
-        const updatedProduct = await ProductService.prototype.updateProduct(id, payload as { name?: string; price?: number, categoryId?: number, active?: boolean })
+        const updatedProduct = await this.service.updateProduct(id, payload as { name?: string; price?: number, categoryId?: number, active?: boolean })
         if (!updatedProduct) {
             reply.status(404).send({ error: "Product not found" })
             return
@@ -40,7 +47,7 @@ export class ProductController {
     }
     async deleteProduct(request: FastifyRequest, reply: FastifyReply) {
         const id = parseInt((request.params as { id: string }).id)
-        const success = await ProductService.prototype.deleteProduct(id)
+        const success = await this.service.deleteProduct(id)
         if (!success) {
             reply.status(404).send({ error: "Product not found" })
             return
